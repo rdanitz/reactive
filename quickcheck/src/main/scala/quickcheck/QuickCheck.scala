532a5f70bf3d6e88c70470ef7dd6b6757ad1d4dd
Robert Danitz <robert.danitz@posteo.de>
2015-04-13 19:39:23 +0100

    adds gitignore and week1 solution

--- quickcheck/src/main/scala/quickcheck/QuickCheck.scala
+++ quickcheck/src/main/scala/quickcheck/QuickCheck.scala
@@ -9,13 +9,29 @@ import Prop._
 
 abstract class QuickCheckHeap extends Properties("Heap") with IntHeap {
 
-  property("min1") = forAll { a: Int =>
-    val h = insert(a, empty)
-    findMin(h) == a
+  property("min1") = forAll { a: A =>
+    findMin(insert(a, empty)) == a
   }
 
-  lazy val genHeap: Gen[H] = ???
+  property("insert1") = forAll { (a: A, h: H) =>
+    findMin(insert(a, h)) == List(a,findMin(h)).min
+  }
+
+  property("meld1") = forAll { (h1: H, h2: H) =>
+    findMin(meld(h1, h2)) == List(findMin(h1), findMin(h2)).min
+  }
+
+  property("deleteMin1") = forAll { (a: A, h: H) =>
+    if (a <= findMin(h))
+      deleteMin(insert(a, h)) == h
+    else
+      deleteMin(insert(a, h)) != h
+  }
+
+  lazy val genHeap: Gen[H] = for {
+    x <- arbitrary[Int]
+    h <- oneOf(const(empty), genHeap)
+  } yield insert(x, h)
 
   implicit lazy val arbHeap: Arbitrary[H] = Arbitrary(genHeap)
-
 }
