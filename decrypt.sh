df366636a90648a4612c14fb448f7bcb2a13db2a
Robert Danitz <robert.danitz@posteo.de>
2015-05-11 12:49:10 +0100

    re-upload suggestion crpyted sources

--- decrypt.sh
+++ decrypt.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-for i in */src/main/scala/*/*.gpg
+for i in */src/main/scala/**/*.gpg
 do
   src=$(echo $i | sed -e 's/.gpg$//g')
   gpg --yes --quiet --decrypt --cipher-algo AES256 --passphrase-file secret.txt --output $src $i
